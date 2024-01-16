ff928232e22d82d676e6d41d606a9ecfc2004daf
DaonYoon <zfydz@naver.com>
2024-01-16 18:09:46 +0900

    first commit

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,23 @@
+import style from "@/app/(afterLogin)/home/home.module.css";
+import Tab from "./_component/Tab";
+import PostForm from "./_component/PostForm";
+import TabProvider from "./_component/TabProvider";
+import Post from "../_component/Post";
+
+export default function Home() {
+  return (
+    <TabProvider>
+      <main className={style.main}>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </main>
+    </TabProvider>
+  );
+}
