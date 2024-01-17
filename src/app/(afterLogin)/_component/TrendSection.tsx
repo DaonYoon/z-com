9388f603576027cd5208203ecb09cf370ee2b212
DaonYoon <zfydz@naver.com>
2024-01-17 15:31:32 +0900

    nextauth까지

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -2,13 +2,20 @@
 
 import style from './trendSection.module.css';
 import Trend from "@/app/(afterLogin)/_component/Trend";
+import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 
 export default function TrendSection() {
   const pathname = usePathname();
+  const {data: session} = useSession();
+
   console.log(pathname)
 
   if(pathname === '/explore') return null;
+
+  if(!session?.user) {
+    return null;
+  }
 
   return (
     <div className={style.trendBg}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import styles from "@/app/(beforeLogin)/_component/main.module.css"
+import styles from "@/app/(beforeLogin)/_component/main.module.css";
 import { MSWComponent } from "./_component/MSWCompont";
+import AuthSession from "./_component/AuthSession";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MSWComponent />
-        <div className={styles.container}>{children}</div>
+        <div className={styles.container}>
+          <AuthSession>{children}</AuthSession>
+        </div>
       </body>
     </html>
   );
